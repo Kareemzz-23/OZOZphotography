42c6aa1565ff5068a7cc5a8230c39b1a5d530380
Kareemzz-23 <trashkareem@gmail.com>
2025-02-01 21:42:17 +0000

    commit

--- scriptnav.js
+++ scriptnav.js
@@ -1,3 +1,12 @@
 document.getElementById('hamburger-menu').addEventListener('click', function() {
     document.getElementById('nav-links').classList.toggle('active');
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
